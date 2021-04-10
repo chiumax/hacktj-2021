edf9563bcb97076e7074ccb004b349b4e3dda74b
Rithvik <rbhogavilli@gmail.com>
2021-04-10 16:37:58 -0400

    Add heat map functionality

--- dashboard/src/components/Mapp.jsx
+++ dashboard/src/components/Mapp.jsx
@@ -28,10 +28,65 @@ const Mapp = () => {
                 "data": "./crashes_2021.geojson"
             });
             console.log("brudda")
+
+            map.addLayer({
+                "id": "crashes-heat",
+                "type": "heatmap",
+                "source": "route",
+                "maxzoom": 16,
+                "paint": {
+                    'heatmap-weight': {
+                        'property': 'dbh',
+                        'type': 'exponential',
+                        'stops': [
+                            [1, 0],
+                            [62, 1]
+                        ]
+                    },
+                    'heatmap-intensity': {
+                        'stops': [
+                            [11, 1],
+                            [15, 3]
+                        ]
+                    },
+                    'heatmap-color': [
+                        'interpolate',
+                        ['linear'],
+                        ['heatmap-density'],
+                        0,
+                        'rgba(33,102,172,0)',
+                        0.2,
+                        'rgb(103,169,207)',
+                        0.4,
+                        'rgb(209,229,240)',
+                        0.6,
+                        'rgb(253,219,199)',
+                        0.8,
+                        'rgb(239,138,98)',
+                        1,
+                        'rgb(178,24,43)'
+                    ],
+                    'heatmap-radius': {
+                        'stops': [
+                            [11, 15],
+                            [15, 20]
+                        ]
+                    },
+                    'heatmap-opacity': {
+                        'default': 1,
+                        'stops': [
+                            [14, 1],
+                            [15, 0]
+                        ]
+                    }
+                }
+            });
+
             map.addLayer({
                 "id": "route",
                 "type": "circle",
                 "source": "route",
+                "minzoom": 15,
                 "paint": {
                     "circle-radius": 10,
                     "circle-color": "#ff0000"
