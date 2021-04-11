603254b84257cec237c5ecda02ec2229a614a97c
chiumax <dumblole@gmail.com>
2021-04-11 16:05:55 +0000

    izza working

--- chakra-dashboard/src/index.js
+++ chakra-dashboard/src/index.js
@@ -1,13 +1,13 @@
 import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import theme from './theme';
 
 ReactDOM.render(
-  <StrictMode>
+  <div>
     <ColorModeScript initialColorMode="dark" />
     <App />
-  </StrictMode>,
+  </div>,
   document.getElementById('root')
 );
